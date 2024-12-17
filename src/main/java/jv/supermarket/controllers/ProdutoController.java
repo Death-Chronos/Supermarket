cf58139bb194ef4b090089c4568a465307ecbeb0
JV <joaovictorcunha12102002@gmail.com>
2024-12-17 10:03:53 -0300

    Documentação inicial
    Configuração da classe Mensagem, como indicativo de classe de resposta, e documentação iniciais de alguns endpoints de Produto.

--- src/main/java/jv/supermarket/controllers/ProdutoController.java
+++ src/main/java/jv/supermarket/controllers/ProdutoController.java
@@ -16,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jv.supermarket.DTOs.ProdutoDTO;
 import jv.supermarket.DTOs.request.ProdutoRequestDTO;
@@ -23,7 +30,7 @@ import jv.supermarket.config.RespostaAPI;
 import jv.supermarket.entities.Produto;
 import jv.supermarket.exceptions.ResourceNotFoundException;
 import jv.supermarket.services.ProdutoService;
-
+import jv.supermarket.services.Exceptions.Mensagem;
 
 @RestController
 @RequestMapping("/supermarket/produto")
@@ -32,6 +39,23 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+    @Operation(summary = "Salva um novo produto", 
+        description = "Recebe uma DTO de produto, que possui em conjunto a(s) categoria(s) em que ele fará parte ( a categoria já deve existir no sistema anteriormente), lembrando que, um produto com o mesmo nome e marca não pode existir")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "409", 
+                description = "Produto com mesmo com o mesmo nome e marca já existe", 
+                content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Mensagem.class))),
+
+            @ApiResponse(responseCode = "404", 
+                description = "Não existe uma categoria com os nomes informados", 
+                content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Mensagem.class))),
+
+            @ApiResponse(responseCode = "201", description = "Produto criado com sucesso",
+                content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Produto.class)))
+    })
     @PostMapping("/save")
     public ResponseEntity<Produto> saveProduto(@RequestBody @Valid ProdutoRequestDTO produto) {
 
@@ -39,11 +63,28 @@ public class ProdutoController {
 
     }
 
+    @Operation(summary = "Busca um produto pelo id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "404", description = "Nenhum produto foi encontrado com o id requisitado", 
+            content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = Mensagem.class))),
+        
+        @ApiResponse(responseCode = "200", description = "Produto encontrado com sucesso", 
+            content = @Content(mediaType = "application/json",
+                schema = @Schema(implementation = Produto.class)))
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Produto> getProdutoById(@PathVariable Long id) {
+    public ResponseEntity<Produto> getProdutoById(@Parameter(description = "Id do produto a ser buscado. Exemplo: 1") @PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(produtoService.getProdutoById(id));
     }
 
+    @Operation(summary = "Busca todos os produtos existentes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", 
+                description = "Produtos encontrados com sucesso",
+                content = @Content(mediaType = "application/json", 
+                    array = @ArraySchema(schema = @Schema(implementation = Produto.class))))
+    })
     @GetMapping("/all")
     public ResponseEntity<List<Produto>> getAllProdutos() {
         return ResponseEntity.status(HttpStatus.OK).body(produtoService.getAllProdutos());
@@ -53,7 +94,7 @@ public class ProdutoController {
     public ResponseEntity<List<Produto>> getProdutosByNome(@RequestParam String nome) {
         List<Produto> produtos = produtoService.getProdutosByNome(nome);
         if (produtos.isEmpty()) {
-            throw new ResourceNotFoundException("Nenhum produto foi encontrado com o Nome: "+ nome);
+            throw new ResourceNotFoundException("Nenhum produto foi encontrado com o Nome: " + nome);
         }
         return ResponseEntity.status(HttpStatus.OK).body(produtos);
     }
@@ -68,13 +109,16 @@ public class ProdutoController {
     }
 
     @GetMapping("/by-marca-and-nome")
-    public ResponseEntity<List<Produto>> getProdutosByMarcaAndNome(@RequestParam String marca, @RequestParam String nome) {
+    public ResponseEntity<List<Produto>> getProdutosByMarcaAndNome(@RequestParam String marca,
+            @RequestParam String nome) {
         List<Produto> produtos = produtoService.getProdutosByMarcaAndNome(marca, nome);
         if (produtos.isEmpty()) {
-            throw new ResourceNotFoundException("Nenhum produto foi encontrado com a Marca: " + marca + " e o Nome: "+ nome);
+            throw new ResourceNotFoundException(
+                    "Nenhum produto foi encontrado com a Marca: " + marca + " e o Nome: " + nome);
         }
         return ResponseEntity.status(HttpStatus.OK).body(produtos);
     }
+
     @GetMapping("/by-categoria-nome")
     public ResponseEntity<List<ProdutoDTO>> getProdutosByCategoriaNome(@RequestParam String nome) {
         List<ProdutoDTO> produtos = produtoService.getProdutosByCategoriaNome(nome);
@@ -94,13 +138,13 @@ public class ProdutoController {
     public ResponseEntity<RespostaAPI> deleteProduto(@PathVariable Long id) {
         produtoService.deleteProdutoById(id);
 
-        return ResponseEntity.status(HttpStatus.OK).body(new RespostaAPI(Instant.now(),"Produto deletado com sucesso"));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new RespostaAPI(Instant.now(), "Produto deletado com sucesso"));
     }
+
     @PutMapping("/{id}/addEstoque")
     public ResponseEntity<Produto> aumentarEstoque(@PathVariable Long id, @RequestParam int quantidade) {
         return ResponseEntity.status(HttpStatus.OK).body(produtoService.addProdutoEstoque(quantidade, id));
     }
-    
-    
 
 }
